85146706ebb672b06ffeded409a6837fa63a6722
Trung Tâm <trantrungtam140602@gmail.com>
2025-03-19 02:41:45 +0700

    lastnhad

--- StudentManagementImpl.java
+++ StudentManagementImpl.java
@@ -33,31 +33,37 @@ public class StudentManagementImpl implements StudentManagement {
     @Override
     public int getNoOfStudent() {
         return students.size();
+        //Trả về số lượng cựu sinh viên có trong danh sách. 
     }
 
     @Override
     public int getNoOfStudent_byGender(String gender) {
         return (int) students.stream().filter(s -> s.getGender().equalsIgnoreCase(gender)).count();
+        //Trả về số lượng cựu sinh viên, lọc theo giá trị gender
     }
 
     @Override
     public int getNoOfStudent_byMajor(String major) {
         return (int) students.stream().filter(s -> s.getMajor().equalsIgnoreCase(major)).count();
+        //Trả về số lượng cựu sinh viên, lọc theo giá trị major.
     }
 
     @Override
     public int getNoOfStudent_byGPA(double gpa) {
         return (int) students.stream().filter(s -> s.getGpa() < gpa).count();
+        //Trả về số lượng cựu sinh viên có điểm GPA nhỏ hơn giá trị cho trước. 
     }
 
     @Override
     public Student findStudent_byName(String name) {
         return students.stream().filter(s -> s.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
+        //Tìm và trả về đối tượng sinh viên theo name, trả về null nếu không tìm thấy.
     }
 
     @Override
     public Student findStudent_byID(String id) {
         return students.stream().filter(s -> s.getId().equals(id)).findFirst().orElse(null);
+        //Tìm và trả về đối tượng sinh viên theo id, trả về null nếu không tìm thấy.
     }
 
     @Override
@@ -65,6 +71,7 @@ public class StudentManagementImpl implements StudentManagement {
         return students.stream()
                 .filter(s -> s.getMajor().equalsIgnoreCase(major))
                 .collect(Collectors.toCollection(ArrayList::new));
+                //Tìm và trả về danh sách sinh viên, lọc theo major.
     }
 
     @Override
@@ -72,11 +79,13 @@ public class StudentManagementImpl implements StudentManagement {
         return students.stream()
                 .filter(s -> s.getGpa() < gpa)
                 .collect(Collectors.toCollection(ArrayList::new));
+                //Tìm và trả về danh sách sinh viên có GPA nhỏ hơn giá trị cho trước. 
     }
 
     @Override
     public double getAvgGPA() {
         return students.stream().mapToDouble(Student::getGpa).average().orElse(0);
+        //Tính và trả về giá trị trung bình của GPA.
     }
 
     @Override
@@ -85,6 +94,7 @@ public class StudentManagementImpl implements StudentManagement {
                 .filter(s -> s.getGender().equalsIgnoreCase(gender))
                 .max(Comparator.comparingDouble(Student::getGpa))
                 .orElse(null);
+                // Tìm và trả về sinh viên có điểm trung bình GPA cao nhất theo giới tính.
     }
 
     @Override
@@ -93,6 +103,7 @@ public class StudentManagementImpl implements StudentManagement {
                 .filter(s -> s.getName().split(" ")[0].equalsIgnoreCase(fname))
                 .max(Comparator.comparingDouble(Student::getGpa))
                 .orElse(null);
+        // Tìm và trả về sinh viên có điểm trung bình GPA cao nhất theo tên (first name).
     }
 
     @Override
@@ -101,6 +112,7 @@ public class StudentManagementImpl implements StudentManagement {
                 .filter(s -> s.getName().split(" ")[1].equalsIgnoreCase(lname))
                 .max(Comparator.comparingDouble(Student::getGpa))
                 .orElse(null);
+                //Tìm và trả về sinh viên có điểm trung bình GPA cao nhất theo họ (last name).
     }
 
     @Override
@@ -109,6 +121,7 @@ public class StudentManagementImpl implements StudentManagement {
                 .filter(s -> s.getMajor().equalsIgnoreCase(major))
                 .max(Comparator.comparingDouble(Student::getGpa))
                 .orElse(null);
+                //Tìm và trả về sinh viên có điểm trung bình GPA cao nhất theo ngành (major).
     }
 
     @Override
@@ -117,6 +130,7 @@ public class StudentManagementImpl implements StudentManagement {
                 .filter(s -> s.getMajor().equalsIgnoreCase(major))
                 .min(Comparator.comparingDouble(Student::getGpa))
                 .orElse(null);
+                //Tìm và trả về sinh viên có điểm trung bình GPA thấp nhất theo ngành (major).
     }
 
     @Override
@@ -125,6 +139,7 @@ public class StudentManagementImpl implements StudentManagement {
                 .sorted(Comparator.comparingDouble(Student::getGpa).reversed())
                 .limit(10)
                 .collect(Collectors.toCollection(ArrayList::new));
+                //Tìm và trả về danh sách Top10 sinh viên có điểm GPA cao nhất.
     }
 
     @Override
@@ -134,6 +149,7 @@ public class StudentManagementImpl implements StudentManagement {
                 .sorted(Comparator.comparingDouble(Student::getGpa).reversed())
                 .limit(10)
                 .collect(Collectors.toCollection(ArrayList::new));
+                //Tìm và trả về danh sách Top10 sinh viên có điểm GPA cao nhất theo giới tính (gender). 
     }
 
     @Override
@@ -143,6 +159,7 @@ public class StudentManagementImpl implements StudentManagement {
                 .sorted(Comparator.comparingDouble(Student::getGpa).reversed())
                 .limit(10)
                 .collect(Collectors.toCollection(ArrayList::new));
+    //Tìm và trả về danh sách Top10 sinh viên có điểm GPA cao nhất theo ngành học (major).
     }
 
     @Override
@@ -152,6 +169,7 @@ public class StudentManagementImpl implements StudentManagement {
                 .sorted(Comparator.comparingDouble(Student::getGpa))
                 .limit(10)
                 .collect(Collectors.toCollection(ArrayList::new));
+                // Tìm và trả về danh sách Last10 sinh viên có điểm GPA thấp nhất theo giới tính (gender).
     }
 
     @Override
@@ -161,6 +179,7 @@ public class StudentManagementImpl implements StudentManagement {
                 .sorted(Comparator.comparingDouble(Student::getGpa))
                 .limit(10)
                 .collect(Collectors.toCollection(ArrayList::new));
+                //Tìm và trả về danh sách Last10 sinh viên có điểm GPA thấp nhất theo ngành học (major).
     }
 
     @Override
@@ -169,5 +188,6 @@ public class StudentManagementImpl implements StudentManagement {
                 .map(Student::getMajor)
                 .distinct()
                 .collect(Collectors.toCollection(ArrayList::new));
+                //Tìm và trả về danh sách các ngành học trong dữ liệu, lưu ý loại bỏ những giá trị trùng nhau. Có thể sử dụng HashSet để loại trừ những giá trị trùng nhau.
     }
 }
